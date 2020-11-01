9f4b90a2dbb8843a8d75d39a1695f0a57de8d149
Nima Shoghi <nimashoghi@gmail.com>
2020-11-01 02:35:49 +0000

    Update

--- bart.py
+++ bart.py
@@ -6,18 +6,16 @@ from torch.optim import AdamW
 from transformers import (
     BartForConditionalGeneration,
     BartTokenizer,
-    get_linear_schedule_with_warmup,
 )
 
 from data import BigPatentDataModule
 
 
 class SummarizerModule(nn.Module):
-    model: BartForConditionalGeneration
-
-    def __init__(self):
+    def __init__(self, tokenizer: BartTokenizer):
         super(SummarizerModule, self).__init__()
 
+        self.tokenizer = tokenizer
         self.model = BartForConditionalGeneration.from_pretrained(
             "facebook/bart-large-cnn"
         )
@@ -28,6 +26,10 @@ class SummarizerModule(nn.Module):
 
         output_ids = input["output_ids"]
         output_mask = input["output_mask"]
+
+        print(
+            input_ids.size(), input_mask.size(), output_ids.size(), output_mask.size()
+        )
 
         output_ids[output_ids[:, :] == self.tokenizer.pad_token_id] = -100
 
@@ -48,7 +50,7 @@ class Summarizer(pl.LightningModule):
         self.hparams = hparams
         self.tokenizer = tokenizer
 
-        self.model = SummarizerModule()
+        self.model = SummarizerModule(self.tokenizer)
 
     def training_step(self, batch, batch_idx):
         output = self.model(batch)
@@ -61,43 +63,13 @@ class Summarizer(pl.LightningModule):
         return dict(val_loss=output["loss"], log=dict(val_loss=output["loss"]))
 
     def configure_optimizers(self):
-        model = self.model
-        no_decay = ["bias", "LayerNorm.weight"]
-        optimizer_grouped_parameters = [
-            {
-                "params": [
-                    p
-                    for n, p in model.named_parameters()
-                    if not any(nd in n for nd in no_decay)
-                ],
-                "weight_decay": self.hparams.weight_decay,
-            },
-            {
-                "params": [
-                    p
-                    for n, p in model.named_parameters()
-                    if any(nd in n for nd in no_decay)
-                ],
-                "weight_decay": 0.0,
-            },
-        ]
         optimizer = AdamW(
-            optimizer_grouped_parameters,
+            self.model.parameters(),
             lr=self.hparams.learning_rate,
             eps=self.hparams.adam_epsilon,
         )
-        t_total = (
-            (10000 // (self.hparams.train_batch_size * max(1, self.hparams.n_gpu)))
-            // self.hparams.gradient_accumulation_steps
-            * float(self.hparams.num_train_epochs)
-        )
-        scheduler = get_linear_schedule_with_warmup(
-            optimizer,
-            num_warmup_steps=self.hparams.warmup_steps,
-            num_training_steps=t_total,
-        )
 
-        return [optimizer], [scheduler]
+        return optimizer
 
 
 #%%
@@ -106,14 +78,12 @@ args_dict = dict(
     weight_decay=0.0,
     adam_epsilon=1e-8,
     warmup_steps=0,
-    train_batch_size=4,
-    eval_batch_size=4,
+    train_batch_size=2,
+    eval_batch_size=2,
     num_train_epochs=2,
     gradient_accumulation_steps=8,
     n_gpu=2,
-    resume_from_checkpoint=None,
     fp_16=True,  # if you want to enable 16-bit training then install apex and set this to true
-    opt_level="O1",  # you can find out more on optimisation levels here https://nvidia.github.io/apex/amp.html#opt-levels-and-properties
     max_grad_norm=1.0,  # if you enable 16-bit training then set this to a sensible value, 0.5 is a good default
     seed=42,
 )
@@ -125,7 +95,6 @@ train_params = dict(
     gpus=args.n_gpu,
     max_epochs=args.num_train_epochs,
     precision=16 if args.fp_16 else 32,
-    resume_from_checkpoint=args.resume_from_checkpoint,
     gradient_clip_val=args.max_grad_norm,
     fast_dev_run=True,
     terminate_on_nan=True,
@@ -137,7 +106,7 @@ if __name__ == "__main__":
 
     tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
     model = Summarizer(args, tokenizer)
-    data_module = BigPatentDataModule(tokenizer)
+    data_module = BigPatentDataModule(tokenizer, batch_size=args.train_batch_size)
 
     trainer = pl.Trainer(
         **train_params,
--- data.py
+++ data.py
@@ -34,7 +34,7 @@ def read_data(split_type: str):
 
 
 class BigPatentDataModule(pl.LightningDataModule):
-    def __init__(self, tokenizer: BartTokenizer, batch_size=32, sequence_length=1024):
+    def __init__(self, tokenizer: BartTokenizer, batch_size=8, sequence_length=1024):
         super(BigPatentDataModule, self).__init__()
 
         self.batch_size = batch_size
@@ -42,16 +42,15 @@ class BigPatentDataModule(pl.LightningDataModule):
         self.tokenizer = tokenizer
 
     def batch_collate(self, batch):
-        print(batch)
         input = self.tokenizer(
-            [input for input, _ in batch],
+            [value["description"] for value in batch],
             max_length=self.sequence_length,
             padding=PaddingStrategy.MAX_LENGTH,
             truncation=TruncationStrategy.LONGEST_FIRST,
             return_tensors=TensorType.PYTORCH,
         )
         output = self.tokenizer(
-            [output for _, output in batch],
+            [value["abstract"] for value in batch],
             max_length=self.sequence_length,
             padding=PaddingStrategy.MAX_LENGTH,
             truncation=TruncationStrategy.LONGEST_FIRST,
