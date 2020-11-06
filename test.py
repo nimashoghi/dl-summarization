3421c459bb4708b5d3ce4f84562fe60710e0d69f
Nima Shoghi <nimashoghi@gmail.com>
2020-11-06 19:30:46 +0000

    Update

--- test.py
+++ test.py
@@ -1,16 +1,20 @@
 #%%
 from transformers import T5Tokenizer
+
 from model import Summarizer
-
-
-tokenizer = T5Tokenizer.from_pretrained("t5-small")
-model: Summarizer = Summarizer.load_from_checkpoint(
-    "lightning_logs/version_10/checkpoints/epoch=1.ckpt", tokenizer=tokenizer
-)
-model
 
 # %%
 with open("input.txt", "r") as f:
     text = f.read()
 
+#%%
+tokenizer = T5Tokenizer.from_pretrained("t5-small")
+model: Summarizer = Summarizer({}, tokenizer=tokenizer)
+model.generate_test(text, max_length=512)
+
+#%%
+tokenizer = T5Tokenizer.from_pretrained("t5-small")
+model: Summarizer = Summarizer.load_from_checkpoint(
+    "lightning_logs/version_10/checkpoints/epoch=1.ckpt", tokenizer=tokenizer
+)
 model.generate_test(text, max_length=512)
