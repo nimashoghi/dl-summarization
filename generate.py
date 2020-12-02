8e70c76c846a0f82e6dbf6fa2ec016f3e276d3ad
Nima Shoghi <nimashoghi@gmail.com>
2020-12-02 01:42:22 +0000

    Update

--- generate.py
+++ generate.py
@@ -11,12 +11,13 @@ scorer = rouge_scorer.RougeScorer(["rouge1", "rouge2", "rougeL"], use_stemmer=Tr
 
 
 #%%
-og_model: PegasusSummarizer = PegasusSummarizer()
+og_model: PegasusSummarizer = PegasusSummarizer(pretrained_name="google/pegasus-large")
 model: LongformerPegasusSummarizer = LongformerPegasusSummarizer.load_from_checkpoint(
     # "/workspaces/summarization-remote/checkpoints-longformer_pegasus-tldr_legal/epoch=124.ckpt",
     # "/workspaces/summarization-remote/checkpoints-bigpatent/checkpoints-best/epoch=80.ckpt"
     # "/workspaces/summarization-remote/checkpoints-longformer_pegasus-big_patent/last.ckpt"
     "/workspaces/summarization-remote/checkpoints-bigpatent/checkpoints-best/epoch=78.ckpt"
+    # "/workspaces/summarization-remote/checkpoints-best-longformer_pegasus-tldr_legal/epoch=113.ckpt"
 )
 
 og_model = og_model.to("cuda:0")
@@ -53,12 +54,16 @@ def generate_text(model, text, max_length=6144, device="cuda:0"):
 #%%
 import random
 
-CPC_CODES = ["a", "b", "c", "d", "e", "f", "g", "h", "y"]
-items = [
-    item
-    for cpc_code in CPC_CODES
-    for item in BigPatentDataset.read_data("test", cpc_code)
-]
+if False:
+    CPC_CODES = ["a", "b", "c", "d", "e", "f", "g", "h", "y"]
+    items = [
+        item
+        for cpc_code in CPC_CODES
+        for item in BigPatentDataset.read_data("test", cpc_code)
+    ]
+else:
+    items = list(TLDRLegalDataset())
+
 random.shuffle(items)
 d = iter(items)
 
@@ -94,10 +99,18 @@ top_length_samples = sorted(items, key=lambda x: len(x["abstract"]), reverse=Tru
 len(items), len(top_length_samples)
 
 #%%
+import pickle
+
+samples = pickle.load(open("/workspaces/summarization-remote/samples-nov23.bin", "rb"))
+type(samples), len(samples)
+
+#%%
 runs_og = []
 runs_us = []
+
 sum_og = {}
 sum_us = {}
+
 count = 0
 
 from threading import Thread
@@ -125,16 +138,17 @@ def run(sample_data, model, runs, device):
     )
 
 
-for sample_data in top_length_samples:
+for sample_data in samples:
     count += 1
-    t1 = Thread(target=run, args=[sample_data, og_model, runs_og, "cuda:0"])
-    t1.start()
+    run(sample_data, og_model, runs_og, "cuda:0")
+    # t1 = Thread(target=run, args=[sample_data, og_model, runs_og, "cuda:0"])
+    # t1.start()
 
-    t2 = Thread(target=run, args=[sample_data, model, runs_us, "cuda:1"])
-    t2.start()
+    # t2 = Thread(target=run, args=[sample_data, model, runs_us, "cuda:1"])
+    # t2.start()
 
-    t1.join()
-    t2.join()
+    # t1.join()
+    # t2.join()
 
     print(".", end="")
 
@@ -157,8 +171,11 @@ data
 #%%
 import pickle
 
-with open("data-nov23-highest-count.bin", "wb") as f:
-    pickle.dump(data, f)
+# with open("data-nov23-pegasus.bin", "wb") as f:
+#     pickle.dump(data, f)
+
+data = pickle.load(open("/workspaces/summarization-remote/data-nov23.bin", "rb"))
+runs_og, runs_us = data["runs_og"], data["runs_us"]
 
 
 #%%
@@ -169,24 +186,24 @@ runs = list(
         sorted(runs_us, key=lambda x: x["abstract_length"], reverse=True),
         sorted(runs_og, key=lambda x: x["abstract_length"], reverse=True),
     )
-)
+)[0:100]
 
 m_us = dict(rouge1=0, rouge2=0, rougeL=0)
 m_them = dict(rouge1=0, rouge2=0, rougeL=0)
-for r1, r2 in itertools.islice(zip(runs_us, runs_og), 1):
-    print(dict(abs=r1["abstract"], r1=r1["generated"], r2=r2["generated"]))
+for r1, r2 in runs:
+    # print(dict(abs=r1["abstract"], r1=r1["generated"], r2=r2["generated"]))
     for metric, value in r1["metrics"].items():
-        m_us[metric] += value.fmeasure
+        m_us[metric] += value.recall
 
     for metric, value in r2["metrics"].items():
-        m_them[metric] += value.fmeasure
+        m_them[metric] += value.recall
 
 m_us, m_them
 
-#%%
+x = sorted(runs_og, key=lambda x: x["abstract_length"], reverse=True)[0:100]
 for metric in set([*m_us.keys(), *m_them.keys()]):
-    average_us = m_us[metric] / len(runs)
-    average_og = m_them[metric] / len(runs)
+    average_us = m_us[metric] / len(x)
+    average_og = m_them[metric] / len(x)
     print(f"[{metric}]: {average_us} us vs. {average_og} pegasus")
 
 # %%
